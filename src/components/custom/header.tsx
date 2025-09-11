544b21799ba7fa692697077e926246c910ee2ac6
MillennialEstoico <millennialestoico@gmail.com>
2025-09-10 21:58:37 -0500

    Quick fix

--- src/components/custom/header.tsx
+++ src/components/custom/header.tsx
@@ -2,7 +2,7 @@
 
 export const Header = () => {
   return (
-    <header className="fixed top-0 left-0 z-50 flex items-center justify-center px-2 sm:px-4 py-2 bg-background text-black dark:text-white w-full">
+    <header className="flex items-center justify-center px-2 sm:px-4 py-2 bg-background text-black dark:text-white w-full">
       <img
         src="/avatar/logo-contenidos.png"
         alt="Logo Contenidos"
